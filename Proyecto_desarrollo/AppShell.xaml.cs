a4b83d76322bc1752b25ff9f5ec3ca0764f6623a
ramos <ramos@DESKTOP-T7T971U>
2025-11-22 23:05:08 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_desarrollo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto_desarrollo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Proyecto_desarrollo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Proyecto_desarrollo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Proyecto_desarrollo.Pages")]
